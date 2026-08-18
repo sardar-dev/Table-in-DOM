92735b62e9b163b4fc39d4009db45508c6dd2b13
sardar <97310586+sardar-dev@users.noreply.github.com>
2026-08-18 22:59:54 +0500

    Implement multiplication table generator
    Add multiplication table generator functionality.

---
+++ app.js
@@ -0,0 +1,24 @@
+ let input = document.querySelector("input");
+      let btn = document.querySelector("button");
+      let output = document.querySelector("p");
+
+      
+        
+
+      function clicks() {
+        let tableof = Number(input.value); //table of
+
+        
+        let result = "";
+        for (let i = 1; i <= 10; i++) {
+          result += ` ${tableof} x  ${i} = ${i * tableof} ` + "<br>";
+
+          input.value = "";
+
+          
+        }
+
+        output.innerHTML = result;
+      }
+     
+    
