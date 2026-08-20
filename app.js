faf9c5194df33031e4c879c8cc1a615556eba1bf
sardar <97310586+sardar-dev@users.noreply.github.com>
2026-08-20 10:09:50 +0500

    Refactor click function to execute and improve messages

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 let input = document.querySelector("input");
-let btn = document.querySelector("button");
 let output = document.querySelector("p");
 
-function clicks() {
+function execute(event) {
+  event.preventDefault();
+
   if (input.value === "" || input.value === " " || input.value === "0") {
-    output.innerHTML = "Enter a valid number";
+    output.innerHTML = "Please enter the valid number";
+
     return;
   }
 
-  let tableof = Number(input.value); // table of
-  let result = "";
+  let number = Number(input.value);
+
+  output.innerHTML = "";
 
   for (let i = 1; i <= 10; i++) {
-    result += ` ${tableof} x  ${i} = ${i * tableof} ` + "<br>";
-    input.value = "";
+    output.innerHTML += `${number} x ${i} = ${number * i} <br> `;
   }
-
-  output.innerHTML = result;
 }
